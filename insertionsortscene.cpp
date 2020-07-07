d1c87ac39855872846422c29e925bcf7febd4925
UnRealG <benwhite2413@gmail.com>
2020-07-07 19:09:48 +1200

    Add Insertion Sort Scene

---
+++ insertionsortscene.cpp
@@ -0,0 +1,6 @@
+#include "insertionsortscene.h"
+
+InsertionSortScene::InsertionSortScene()
+{
+
+}
---
+++ insertionsortscene.h
@@ -0,0 +1,12 @@
+#ifndef INSERTIONSORTSCENE_H
+#define INSERTIONSORTSCENE_H
+
+
+class InsertionSortScene : public SortScene
+{
+    Q_OBJECT
+public:
+    InsertionSortScene();
+};
+
+#endif // INSERTIONSORTSCENE_H
